70f8f55c836bcc9db61bb98a2aaa103f59d051ed
Matthias Scheffelmeier <matthias.scheffelmeier@continental.com>
2025-04-04 12:24:38 +0200

    re-export rayon prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 #![cfg_attr(RUSTC_IS_NIGHTLY, feature(test))]
 
-pub(crate) mod qvd_structure;
-pub mod types;
-pub(crate) mod reader;
 pub mod error;
+pub(crate) mod qvd_structure;
+pub(crate) mod reader;
+pub mod types;
 
-pub use types::{QvdDocument, Header, Column, CellValue};
+pub use types::{CellValue, Column, Header, QvdDocument};
 
-    
+pub use rayon::prelude::*;
 
 #[cfg(all(RUSTC_IS_NIGHTLY, test))]
 mod tests {
@@ -15,38 +15,30 @@ mod tests {
 
     extern crate test;
 
-
     #[bench]
-    fn read_test_file_to_row_iter(b: &mut test::Bencher) {        
+    fn read_test_file_to_row_iter(b: &mut test::Bencher) {
         let qvd = QvdDocument::read("tests/big_file.qvd").unwrap();
         b.iter(|| {
             let mut rows = qvd.rows();
-            while let Some(row) = rows.next() {
-                
-            }
+            while let Some(row) = rows.next() {}
         })
     }
 
     #[bench]
-    fn read_test_file_to_row_iter_par(b: &mut test::Bencher) {        
+    fn read_test_file_to_row_iter_par(b: &mut test::Bencher) {
         let qvd = QvdDocument::read("tests/big_file.qvd").unwrap();
         b.iter(|| {
             let mut rows = qvd.rows_par();
-            while let Some(row) = rows.next() {
-                
-            }
+            while let Some(row) = rows.next() {}
         })
     }
 
     #[bench]
-    fn read_test_file_to_row_iter_alt(b: &mut test::Bencher) {        
+    fn read_test_file_to_row_iter_alt(b: &mut test::Bencher) {
         let qvd = QvdDocument::read("tests/big_file.qvd").unwrap();
         b.iter(|| {
             let mut rows = qvd.rows_alt();
-            while let Some(row) = rows.next() {
-                
-            }
+            while let Some(row) = rows.next() {}
         })
     }
-
 }
